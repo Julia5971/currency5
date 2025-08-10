70f3c48fd8a635bb2cfcf7f8b482d31bd9bfd7a4
Julia Kim <happykhy5971@gmail.com>
2025-08-10 16:30:39 +0900

    feat: Jest 환경 설정 및 첫 기능(1.1.1) TDD 사이클 완료

---
+++ src/services/ExchangeRateService.js
@@ -0,0 +1,14 @@
+// src/services/ExchangeRateService.js
+
+/**
+ * 지금은 테스트를 통과시키기 위한 임시 함수입니다.
+ * 나중에 실제 API를 호출하는 로직으로 대체될 것입니다.
+ */
+export const fetchExchangeRates = async () => {
+    return {
+      base: 'USD',
+      rates: {
+        KRW: 1350.25, // 테스트를 통과시킬 임의의 숫자
+      },
+    };
+  };
